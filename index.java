7c956e8865ef96a75ee4e2c4fcd030ed6b478d3c
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2017-12-24 08:59:03 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	
+	int c=3;
+	 if(c<0) {
+		 System.out.println("negative");
+	 }
+	 if(c>0) {
+		 System.out.println("positive");
+	 }
+	 if(c==0) {
+		 System.out.println("zero");
+	 }
+	}
+}
