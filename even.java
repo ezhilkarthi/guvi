d0340e03135b362784b56fca24b84e92ec81f666
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2017-12-24 09:57:08 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class guvi1 {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner a =new Scanner (System.in);
+	int c=a.nextInt();
+	if (c%2==0) {
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+	}
+
+}
+}
