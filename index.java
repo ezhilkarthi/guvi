1de15c2c2f0d9227e94d3eea6f9d957c92476be9
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2017-12-24 09:13:40 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,12 +1,7 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
+public class guvi1 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
@@ -23,4 +18,5 @@ class Ideone
 		 System.out.println("zero");
 	 }
 	}
+
 }
