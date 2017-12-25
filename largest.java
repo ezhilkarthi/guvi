d5d7dad6cf397e354581b8fe49eff6f43c0dfd01
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2017-12-25 09:33:25 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class guvi1 {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner a =new Scanner (System.in);
+		System.out.println("enter any three no");
+		int b=a.nextInt();
+		int c=a.nextInt();
+		int d=a.nextInt();
+		if((b>=c)&&(b>=d)) {
+			System.out.println(b);
+		}
+		if((c>=b)&&(c>=d)) {
+			System.out.println(c);
+		}
+		if((d>=b)&&(d>=c)) {
+			System.out.println(d);
+		}
+		
+		}
+
+}
