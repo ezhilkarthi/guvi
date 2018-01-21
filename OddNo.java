3240b332ed7a463fe5926507697b828bbf71484c
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2018-01-21 12:33:55 +0530

    Add files via upload

---
+++ OddNo.java
@@ -0,0 +1,18 @@
+package displayOddNoInterval;
+import java.util.*;
+public class OddNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a+1;i<=b-1;i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+				
+			}
+		}
+	}
+
+}
