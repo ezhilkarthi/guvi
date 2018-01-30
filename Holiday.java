ea54947f94e964d9c4ca41563c010cd049125234
ezhilkarthi <31158252+ezhilkarthi@users.noreply.github.com>
2018-01-30 21:48:45 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,36 @@
+package holiday;
+import java.util.*;
+public class Holiday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+	    String st=s.next();
+	    switch(st) {
+	    case "monday":
+	    	System.out.println("no");
+	    	break;
+	    case "tuesday":
+	    	System.out.println("no");
+	    	break;
+	    case "wednesday":
+	    	System.out.println("no");
+	    	break;
+	    case "thursday":
+	    	System.out.println("no");
+	    	break;
+	    case "friday":
+	    	System.out.println("no");
+	    	break;
+	    case "saturday":
+	    	System.out.println("yes");
+	    	break;
+	    case "sunday":
+	    	System.out.println("yes");
+	    	break;
+	    default: 	System.out.println("invalid input");
+
+	    }
+	}
+
+}
